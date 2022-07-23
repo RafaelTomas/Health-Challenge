683780b43ef28d5f8f794664898e32a026ac1ee2
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-07-23 12:27:56 -0300

    test(patient): create list and create test

--- src/app/patient/specs/patient.controller.spec.ts
+++ src/app/patient/specs/patient.controller.spec.ts
@@ -1,20 +1,115 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreatePatientDto } from '../dto/create-patient.dto';
+import { Patient } from '../entities/patient.entity';
 import { PatientController } from '../patient.controller';
 import { PatientService } from '../patient.service';
 
+const newPatient = new Patient({
+  name: 'create',
+  healthInsuranceCardId: '123465789',
+  address: 'somewhere',
+  createdAt: '9999-99-99',
+});
+
+const patientList: Patient[] = [
+  new Patient({
+    name: 'test 1',
+    healthInsuranceCardId: '123465789',
+    address: 'somewhere',
+    createdAt: '9999-99-99',
+  }),
+  new Patient({
+    name: 'test 2',
+    healthInsuranceCardId: '123465789',
+    address: 'somewhere',
+    createdAt: '9999-99-99',
+  }),
+  new Patient({
+    name: 'test 3',
+    healthInsuranceCardId: '123465789',
+    address: 'somewhere',
+    createdAt: '9999-99-99',
+  }),
+];
+
+const updatedPatient = new Patient({
+  name: 'update',
+  healthInsuranceCardId: '123465789',
+  address: 'somewhere',
+  createdAt: '9999-99-99',
+});
+
 describe('PatientController', () => {
-  let controller: PatientController;
+  let patientController: PatientController;
+  let patientService: PatientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PatientController],
-      providers: [PatientService],
+      providers: [
+        {
+          provide: PatientService,
+          useValue: {
+            create: jest.fn().mockResolvedValue(newPatient),
+            findAll: jest.fn().mockResolvedValue(patientList),
+            findOne: jest.fn().mockResolvedValue(patientList[0]),
+            update: jest.fn().mockResolvedValue(updatedPatient),
+            remove: jest.fn().mockResolvedValue(undefined),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<PatientController>(PatientController);
+    patientController = module.get<PatientController>(PatientController);
+    patientService = module.get<PatientService>(PatientService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(patientController).toBeDefined();
+    expect(patientService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should return new patient successfully', async () => {
+      const body: CreatePatientDto = {
+        name: 'create',
+        healthInsuranceCardId: '123465789',
+        address: 'somewhere',
+        createdAt: '9999-99-99',
+      };
+
+      const result = await patientController.create(body);
+
+      expect(result).toEqual(newPatient);
+      expect(patientService.create).toHaveBeenCalledTimes(1);
+      expect(patientService.create).toHaveBeenCalledWith(body);
+    });
+    it('should bring in case of error', () => {
+      const body: CreatePatientDto = {
+        name: 'create',
+        healthInsuranceCardId: '123465789',
+        address: 'somewhere',
+        createdAt: '9999-99-99',
+      };
+      jest.spyOn(patientService, 'create').mockRejectedValueOnce(new Error());
+
+      expect(patientController.create(body)).rejects.toThrowError();
+    });
+  });
+
+  describe('list', () => {
+    it('should return patient list successfully', async () => {
+      const result = await patientController.findAll();
+
+      expect(result).toEqual(patientList);
+      expect(typeof result).toEqual('object');
+      expect(patientService.findAll).toHaveBeenCalledTimes(1);
+    });
+
+    it('should bring in case of error', () => {
+      jest.spyOn(patientService, 'findAll').mockRejectedValueOnce(new Error());
+
+      expect(patientController.findAll()).rejects.toThrowError();
+    });
   });
 });
