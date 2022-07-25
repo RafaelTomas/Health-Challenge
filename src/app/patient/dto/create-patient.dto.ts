e18a75ea88e6ad487bb6903213a69f6e9a8fee00
Rafael Tomás <rafaeltomassouza@gmail.com>
2022-07-24 22:59:51 -0300

    fix(patient): send all body

--- src/app/patient/dto/create-patient.dto.ts
+++ src/app/patient/dto/create-patient.dto.ts
@@ -1,27 +1,34 @@
 import { ApiProperty, ApiResponseProperty } from '@nestjs/swagger';
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreatePatientRequestDto {
   @ApiProperty({
     example: 'Matilde Fernandes Gonçalves',
     description: 'Patient name',
   })
+  @IsString()
   name: string;
 
   @ApiProperty({
     example: '123465789',
     description: 'Patient health Insurance Card Id',
   })
+  @IsString()
   healthInsuranceCardId: string;
 
   @ApiProperty({
     example: 'Rua Luís de Castro, 1182',
     description: 'Patient address',
   })
+  @IsString()
   address: string;
 
   @ApiProperty({
     example: '2022-05-05',
     description: 'When it was created in the patient list',
   })
+  @IsString()
+  @IsNotEmpty()
   createdAt: string;
 }
 
